b2813553266ea5892703d257fd63c865b99e6886
Andrew <drace.davidson@gmail.com>
2024-02-08 17:42:00 -0700

    added butones!

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -1,9 +1,24 @@
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
+const darkmodeBtn = document.querySelector('#darkModeBtn');
+const ctaButton = document.querySelector('.cta-button');
 
+
+// Event listener for hamburger menu
 hamButton.addEventListener('click', () => {
     navigation.classList.toggle('open');
     hamButton.classList.toggle('open');
+});
+
+// Event listener for 'Dark Mode' button
+darkmodeBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode')
+});
+
+// Event listener for 'Call To Action' button
+ctaButton.addEventListener('click', () => {
+    // Handle click event for 'Join chamber'
+    joinMember(); // Assuming you have a function for this action
 });
 
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
@@ -24,3 +39,8 @@ document.addEventListener('DOMContentLoaded', () => {
         pTag.innerHTML = pTag.innerHTML.replace('[visits]', visits);
     }
 });
+
+function joinMember() {
+    // Logic for joining member goes here
+    console.log("Joining member...");
+}
