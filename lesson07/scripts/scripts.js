2a120ca42cf9d5d5381717984478bdca117600b6
Andrew <drace.davidson@gmail.com>
2024-02-22 07:45:40 -0700

    lazy load activity

---
+++ lesson07/scripts/scripts.js
@@ -0,0 +1,26 @@
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
+
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+
+// Assuming you have a way to calculate or get site visits
+let visits = 123; // Placeholder for visits
+
+document.addEventListener('DOMContentLoaded', () => {
+    const rightsSection = document.querySelector("#rights");
+    const paragraphs = rightsSection.querySelectorAll("p");
+    const now = new Date();
+    const dateString = now.toLocaleString();
+    paragraphs[1].textContent = `Date and Time: ${dateString}`;
+    document.querySelector("[data-modified-date]").textContent = document.lastModified;
+
+    let pTag = document.querySelector('.card p'); // More precise selection of p tag
+    if(pTag && visits) {
+        pTag.innerHTML = pTag.innerHTML.replace('[visits]', visits);
+    }
+});
