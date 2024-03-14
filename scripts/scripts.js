71498b000abf202ea07aa4f1457dda22ccb8995e
Andrew <drace.davidson@gmail.com>
2024-03-14 16:52:47 -0600

    weatherly dua

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -64,3 +64,31 @@ function displayLinks(data) {
 }
 
 getLinks();
+
+async function getWeatherData() {
+
+    const url = 'https://api.openweathermap.org/data/2.5/weather?lat=40.377937&lon=-111.803055&appid=5a0d88a901126cc81d618d978ab200cb&units=imperial';
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    displayResults(data);
+}
+
+function displayResults(data){
+    // Kelvin to Celsius conversion
+    const temperature = data.main.temp;
+    const description = data.weather[0].description;
+    const iconUrl = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+
+    const currentTemp = document.querySelector('#current-temp');
+    const weatherIcon = document.querySelector('#weather-icon');
+    const captionDesc = document.querySelector('figcaption');
+
+    currentTemp.textContent = temperature.toFixed(2);
+    weatherIcon.src = iconUrl;
+    captionDesc.textContent = description;
+}
+
+// Call function
+getWeatherData();
